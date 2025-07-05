6b1b81b86d20e43636c3a7563d754d63dd4c1f34
james00000007 <95303830+james00000007@users.noreply.github.com>
2025-07-05 02:31:54 -0700

    Update config.py

--- gui-deprecated/_config.py
+++ gui-deprecated/_config.py
@@ -1,4 +1,4 @@
-parent_id = "2eVHbS_yE9kzLrUPN3oX0ACv4gYiM2WT"
-db_uri = "mongodb+srv://blahblahblah.mongodb.net/?retryWrites=true&w=majority"
+parent_id = "1Gi2pOdBaZ67tVUQUA6yfNF8Np1T5FAA_"
+db_uri = "mongodb+srv://dreamspark:lxgd3668au@sa-drive.zcov5ek.mongodb.net/?retryWrites=true&w=majority&appName=sa-drive"
 username_for_logging_into_sa_drive = 'admin'
 password_for_logging_into_sa_drive = 'admin'
